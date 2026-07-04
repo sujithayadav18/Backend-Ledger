6e89641fb6a0d67eca4b9941ffcb30dbc12775ec
sujithayadav18 <ssujithayadavsutrapu@gmail.com>
2026-07-04 18:23:36 +0530

    Improved the structure and added folders and started with auth development

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import userModel from "../models/user.model.js";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken"
 
 async function registerUserController(req: { body: { username: string; email: string; password: string; }; }, res: any) {
   const { username, email, password } = req.body;
@@ -10,6 +11,8 @@ async function registerUserController(req: { body: { username: string; email: st
     return res.status(422).json({ message: "Username or email already exists", status: "failed"});
   }
   const user = await userModel.create({ username, email, password });
-}  
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET as string, { expiresIn: "3d" });
+  res.status(201).json({ message: "User registered successfully", status: "success", token });
+}
 
 export { registerUserController };
